492aa90bb2943e3179e32e4a72b9896435ac1b33
SmiCondctr5200 <78669221+AdenGalang@users.noreply.github.com>
2026-04-09 11:28:46 +0700

    Update version

--- MVVM/ViewModel/AboutViewModel.cs
+++ MVVM/ViewModel/AboutViewModel.cs
@@ -5,11 +5,11 @@ namespace PassManaAlpha.MVVM.ViewModel
     public class AboutViewModel : ForkObject
     {
         public string AppName => "PassMana";
-        public string Version => "v0.1.3";
+        public string Version => "v0.1.4";
         public string Description => "A not very lightweight encrypted password manager built with WPF and .NET 8.";
         public string Author => "Minamorin_SmiCondctr5200";
         public string WaifuImage => "/assets/kazusa_r.jpg";
         public string WaifuName => "Kazusa Kyouyama";
-        public string WaifuSeries => "(杏山カズサ";
+        public string WaifuSeries => "杏山カズサ";
     }
 }
